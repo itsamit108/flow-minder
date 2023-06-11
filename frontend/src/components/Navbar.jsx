e4f8226334cec4311b67b6367984331e7be7884e
itsamit108 <itsamit108@outlook.com>
2023-06-11 17:04:46 +0530

    update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <a className="navbar-brand text-white" href="/">Task Manager</a>
+                    <a className="navbar-brand text-white" href="/">Flow Minder</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
